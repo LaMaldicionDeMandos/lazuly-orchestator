c2a0e8c174f6846a7e315cfa9f05d4405426be41
Marcelo Pasut <pasutmarcelo@gmail.com>
2018-04-25 17:08:22 -0300

    esqueleto de la app

---
+++ src/main/java/co/lazuly/orchestrator/Application.java
@@ -0,0 +1,116 @@
+package co.lazuly.orchestrator;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.google.common.collect.Lists;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.method.annotation.RequestParamMapMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.springframework.http.MediaType.ALL;
+
+/**
+ * Created by boot on 9/5/17.
+ */
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+@EnableEurekaClient
+@EnableResourceServer
+@EnableFeignClients
+public class Application extends WebMvcConfigurerAdapter {
+    private final static Logger log = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Primary
+    @Bean
+    RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<HttpMessageConverter<?>> messageConverters = Lists.newArrayList();
+        MappingJackson2HttpMessageConverter jsonMessageConverter = new MappingJackson2HttpMessageConverter();
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        jsonMessageConverter.setObjectMapper(mapper);
+        messageConverters.add(jsonMessageConverter);
+        restTemplate.setMessageConverters(messageConverters);
+        return restTemplate;
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+
+        converters.add(this.getLowerCaseMessageConverter());
+    }
+
+    @Override
+    public void configureHandlerExceptionResolvers(List<HandlerExceptionResolver> exceptionResolvers) {
+        super.configureHandlerExceptionResolvers(exceptionResolvers);
+
+        final ExceptionHandlerExceptionResolver exceptionHandlerExceptionResolver = new ExceptionHandlerExceptionResolver();
+        final List<HttpMessageConverter<?>> messageConverters = exceptionHandlerExceptionResolver
+                .getMessageConverters();
+        messageConverters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        super.addArgumentResolvers(argumentResolvers);
+        argumentResolvers.add(new RequestParamMapMethodArgumentResolver());
+    }
+
+    private MappingJackson2HttpMessageConverter getLowerCaseMessageConverter() {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE);
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
+        converter.setSupportedMediaTypes(Arrays.asList(ALL));
+
+        return converter;
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // turn off all suffix pattern matching
+        configurer.setUseSuffixPatternMatch(false);
+        // OR
+        // turn on suffix pattern matching ONLY for suffixes
+        // you explicitly register using
+        // configureContentNegotiation(...)
+        //configurer.setUseRegisteredSuffixPatternMatch(true);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
+}
---
+++ src/main/java/co/lazuly/orchestrator/security/ResourceServerConfiguration.java
@@ -0,0 +1,18 @@
+package co.lazuly.orchestrator.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+/**
+ * Created by boot on 7/8/17.
+ */
+@Configuration
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+            .antMatchers( "/**")
+            .authenticated();
+    }
+}
